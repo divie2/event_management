f3657be7ec0cba2f4bce8ca817ba1b83813ff95d
unknown <divine41744@gmail.com>
2025-05-21 16:09:07 +0100

    added accounts

---
+++ event_manager/accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.password_validation import validate_password
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'full_name', 'phone_number', 'password']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            full_name=validated_data['full_name'],
+            phone_number=validated_data['phone_number'],
+            username=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ event_manager/accounts/views.py
@@ -0,0 +1,30 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
